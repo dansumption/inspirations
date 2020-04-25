acb88650e1fd1050293dd010c745d0ef28c38037
Dan Sumption <dan.sumption@autodesk.com>
2020-04-25 19:23:14 +0100

    Update index text

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>inspirations</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          inspirations
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Find something to do
         </p>
 
         <div className="grid">
